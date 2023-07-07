e79538ceeeba1a23bcbaf4ceb8727790ef246fa1
Oleksandr Raduka <89737800+rainb00w@users.noreply.github.com>
2023-07-07 12:48:19 +0300

    _v2d

--- src/app/page.js
+++ src/app/page.js
@@ -25,13 +25,6 @@ export default function Home() {
             <img src="/big-hall.jpeg" alt="big hall" />
           </div>
 
-          <div className="my-8 flex justify-center items-center flex-col ">
-            <p>Лінк щоб додати календар собі -</p>
-            <p>
-              https://calendar.google.com/calendar/ical/tarasovska.studio%40gmail.com/public/basic.ics
-            </p>
-          </div>
-
           <iframe
             src="https://calendar.google.com/calendar/embed?src=tarasovska.studio%40gmail.com&ctz=Europe%2FKiev"
             style={{ border: '0' }}
@@ -42,13 +35,6 @@ export default function Home() {
           ></iframe>
 
           <h2 className="font-bold my-5 text-3xl mt-20">Малий зал 20м</h2>
-
-          <div className="my-8 flex justify-center items-center flex-col ">
-            <p>Лінк щоб додати календар собі -</p>
-            <p className="w-[600px]">
-              https://calendar.google.com/calendar/ical/eb09ccf067b7a5e2c0cc56ea4ba3e4030ccc00818ed70ed9c9399e00a0aae324%40group.calendar.google.com/public/basic.ics
-            </p>
-          </div>
 
           <iframe
             src="https://calendar.google.com/calendar/embed?src=eb09ccf067b7a5e2c0cc56ea4ba3e4030ccc00818ed70ed9c9399e00a0aae324%40group.calendar.google.com&ctz=Europe%2FKiev"
